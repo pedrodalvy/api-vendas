77476979cd36b29a3849a34d3cbe9bcbcdec72fd
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-02 14:58:07 -0400

    feat: add routes file with example route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log('🚀');
+import express from 'express';
+import cors from 'cors';
+import routes from '@shared/http/routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3333. 🚀');
+});
