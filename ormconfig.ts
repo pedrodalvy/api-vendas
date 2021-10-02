f5b51a931b92f32e372bebd8eecbad9a733324e5
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-02 18:26:59 -0400

    chore: config typeorm migrations

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,8 +1,12 @@
-module.exports = {
+export default {
   type: process.env.DB_CONNECTION,
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  migrations: ['./src/shared/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/shared/typeorm/migrations',
+  },
 };
