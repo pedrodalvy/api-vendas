b94725b03cf78f74dc55de0b0e44a74d7e5f4729
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-02 18:18:28 -0400

    chore: config typeorm

---
+++ ormconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  type: process.env.DB_CONNECTION,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+};
