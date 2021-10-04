6f65f2461e5755e598efde3156592c34fd11a5e0
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-04 19:44:06 -0400

    feat: add CreateProductService service

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
+import { AppError } from '@shared/errors/AppError';
+import { Product } from '@modules/products/typeorm/entities/Product';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const repository = getCustomRepository(ProductsRepository);
+    const productExists = await repository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name.');
+    }
+
+    const product = repository.create({ name, price, quantity });
+
+    await repository.save(product);
+
+    return product;
+  }
+}
