05c392ba4a503ccfd340f0b81573f0fb772f426c
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-04 22:25:09 -0400

    feat: add validate to route params and body in products routes file

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,51 @@
 import { Router } from 'express';
 import { ProductsController } from '@modules/products/controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.index);
-productsRouter.post('/', productsController.create);
-productsRouter.get('/:id', productsController.show);
-productsRouter.put('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.create,
+);
+
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+  }),
+  productsController.show,
+);
+
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.update,
+);
+
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+  }),
+  productsController.delete,
+);
 
 export default productsRouter;
