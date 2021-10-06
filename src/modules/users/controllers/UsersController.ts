17efcf8bdd4454f1635b00f8cae30c9573223148
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-05 21:40:31 -0400

    feat: add Users controller

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { ListUserService } from '@modules/users/services/ListUserService';
+import { CreateUserService } from '@modules/users/services/CreateUserService';
+
+export class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const service = new ListUserService();
+    const users = await service.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const service = new CreateUserService();
+    const user = await service.execute({ name, email, password });
+
+    return response.json(user);
+  }
+}
