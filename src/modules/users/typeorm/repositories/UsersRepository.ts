4679d8ac5b1e654bd80674ba3a7de167935d19f4
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-05 21:10:35 -0400

    feat: add Users repository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,17 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '@modules/users/typeorm/entities/User';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    return this.findOne({ where: { name } });
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    return this.findOne({ where: { id } });
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return this.findOne({ where: { email } });
+  }
+}
