24e8710245761181691a3823b47407b4d8b3bf7c
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-06 21:08:01 -0400

    feat: override request adding user id

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -2,6 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import { AppError } from '@shared/errors/AppError';
 import { verify } from 'jsonwebtoken';
 
+interface ITokenPayload {
+  iat: number;
+  ext: number;
+  sub: string;
+}
+
 export function isAuthenticated(
   request: Request,
   response: Response,
@@ -16,7 +22,13 @@ export function isAuthenticated(
   const token = authHeader.split(' ').pop();
 
   try {
-    verify(String(token), String(process.env.SECRET_KEY));
+    const decodedToken = verify(String(token), String(process.env.SECRET_KEY));
+    const { sub } = decodedToken as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch {
     throw new AppError('Invalid JWT Token.');
