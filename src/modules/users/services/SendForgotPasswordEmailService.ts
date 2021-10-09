4f2edbc6786e3b5c1e9a71d23c0e5ae84a802fcb
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-09 15:52:46 -0400

    feat: add SendForgotPasswordEmail service

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '@modules/users/typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '@modules/users/typeorm/repositories/UserTokensRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  email: string;
+}
+
+export class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const userRepository = getCustomRepository(UsersRepository);
+
+    const user = await userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+    const token = await userTokenRepository.generate(user.id);
+
+    // TODO Implements send email
+    console.log(token);
+  }
+}
