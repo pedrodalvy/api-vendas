cf19bf609d442ab9966e2414bde4e885ae3f66d5
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-09 16:35:54 -0400

    feat: add ForgotPassword controller

---
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { SendForgotPasswordEmailService } from '@modules/users/services/SendForgotPasswordEmailService';
+
+export class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const service = new SendForgotPasswordEmailService();
+    await service.execute({ email });
+
+    return response.status(204);
+  }
+}
