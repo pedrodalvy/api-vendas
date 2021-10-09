d402e74fb2d30447b583b1303b10e22e8f2702a4
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-09 10:21:12 -0400

    feat: add update avatar on user routes

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,9 +2,15 @@ import { Router } from 'express';
 import { UsersController } from '@modules/users/controllers/UsersController';
 import { celebrate, Joi, Segments } from 'celebrate';
 import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated';
+import { UserAvatarController } from '@modules/users/controllers/UserAvatarController';
+import multer from 'multer';
+import { uploadConfig } from '@config/upload';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 
@@ -18,6 +24,13 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
 );
 
 export default usersRouter;
