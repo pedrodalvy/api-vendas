32dcd2d579dceafdabcd06568acdee9e6cc9b290
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-10 14:19:28 -0400

    feat: add ShowCustomer service

---
+++ src/modules/customers/services/ShowCustomerService.ts
@@ -0,0 +1,21 @@
+import { Customer } from '@modules/customers/typeorm/entities/Customer';
+import { getCustomRepository } from 'typeorm';
+import { CustomersRepository } from '@modules/customers/typeorm/repositories/CustomersRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  customerId: string;
+}
+
+export class ShowCustomerService {
+  public async execute({ customerId }: IRequest): Promise<Customer> {
+    const repository = getCustomRepository(CustomersRepository);
+    const customer = await repository.findById(customerId);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    return customer;
+  }
+}
