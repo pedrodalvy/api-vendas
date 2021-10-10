bae7360762185fa87d193a91026a31a2694c59cf
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-10 13:58:16 -0400

    feat: add Customers repository

---
+++ src/modules/customers/typeorm/repositories/CustomersRepository.ts
@@ -0,0 +1,17 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Customer } from '@modules/customers/typeorm/entities/Customer';
+
+@EntityRepository(Customer)
+export class CustomersRepository extends Repository<Customer> {
+  public async findByName(name: string): Promise<Customer | undefined> {
+    return this.findOne({ where: { name } });
+  }
+
+  public async findById(id: string): Promise<Customer | undefined> {
+    return this.findOne({ where: { id } });
+  }
+
+  public async findByEmail(email: string): Promise<Customer | undefined> {
+    return this.findOne({ where: { email } });
+  }
+}
