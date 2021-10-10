d7d0d2714ebfad862ce625171ca61f3cc2171672
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-10 14:29:07 -0400

    feat: add UpdateCustomer service

---
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -0,0 +1,36 @@
+import { Customer } from '@modules/customers/typeorm/entities/Customer';
+import { getCustomRepository } from 'typeorm';
+import { CustomersRepository } from '@modules/customers/typeorm/repositories/CustomersRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  customerId: string;
+  name: string;
+  email: string;
+}
+
+export class UpdateCustomerService {
+  public async execute({
+    customerId,
+    name,
+    email,
+  }: IRequest): Promise<Customer> {
+    const repository = getCustomRepository(CustomersRepository);
+    const customerExists = await repository.findByEmail(email);
+
+    if (customerExists && customerExists.email !== email) {
+      throw new AppError('There is already one customer with this email.');
+    }
+
+    const customer = await repository.findById(customerId);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    customer.email = email;
+    customer.name = name;
+
+    return await repository.save(customer);
+  }
+}
