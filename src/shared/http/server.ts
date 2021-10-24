6e43d06444a9cffebb87b8cbf31b60a5a954c583
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-24 16:37:21 -0400

    refactor: add rate limiter on routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,13 +9,15 @@ import { AppError } from '@shared/errors/AppError';
 import '@shared/typeorm';
 import { uploadConfig } from '@config/upload';
 import { pagination } from 'typeorm-pagination';
+import rateLimiter from '@shared/http/middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use(rateLimiter);
 app.use(pagination);
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
