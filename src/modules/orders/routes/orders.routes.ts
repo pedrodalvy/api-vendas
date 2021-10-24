88c6c053ebc7e68d3a961e305e8cddfd61ddfe9a
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-24 11:42:10 -0400

    feat: add orders routes

---
+++ src/modules/orders/routes/orders.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { OrdersController } from '@modules/orders/controllers/OrdersController';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated';
+
+const ordersRouter = Router();
+const ordersController = new OrdersController();
+
+ordersRouter.use(isAuthenticated);
+
+ordersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: { id: Joi.string().uuid().required() },
+  }),
+  ordersController.show,
+);
+
+ordersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      customer_id: Joi.string().uuid().required(),
+      products: Joi.array()
+        .items(
+          Joi.object({
+            id: Joi.string().uuid().required(),
+            quantity: Joi.number().required(),
+          }),
+        )
+        .required(),
+    },
+  }),
+  ordersController.create,
+);
+
+export default ordersRouter;
