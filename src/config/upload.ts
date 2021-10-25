5609143c2aef9e1a3aa784b9f2ad631e4a9a436f
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-25 09:58:27 -0400

    feat: add disk storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,11 +3,13 @@ import multer from 'multer';
 import * as crypto from 'crypto';
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export const uploadConfig = {
   directory: uploadFolder,
+  tmpFolder,
   storage: multer.diskStorage({
-    destination: uploadFolder,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
 
