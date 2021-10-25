6fd886c7e979271e198344309c294b16087a9aa5
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-25 16:49:03 -0400

    chore: create CustomersRepository dependency injection

---
+++ src/shared/container/index.ts
@@ -0,0 +1,8 @@
+import { container } from 'tsyringe';
+import { CustomersRepository } from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
+import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
+
+container.registerSingleton<ICustomersRepository>(
+  'CustomersRepository',
+  CustomersRepository,
+);
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 import routes from '@shared/infra/http/routes';
 import { AppError } from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
+import '@shared/container';
 import { uploadConfig } from '@config/upload';
 import { pagination } from 'typeorm-pagination';
 import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
