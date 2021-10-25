48c6505519df9d327e39c79685cf8af7e9683d0b
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-25 15:21:40 -0400

    refactor: add domain models to order and product modules

---
+++ src/modules/orders/domain/models/IOrder.ts
@@ -0,0 +1,11 @@
+import { ICustomer } from '@modules/customers/domain/models/ICustomer';
+import { ICreateOrderProducts } from '@modules/orders/domain/models/ICreateOrderProducts';
+
+export interface IOrder {
+  id: string;
+  order: number;
+  customer: ICustomer;
+  order_products: ICreateOrderProducts[];
+  created_at: Date;
+  updated_at: Date;
+}
