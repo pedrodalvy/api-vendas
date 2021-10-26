110ac96affb13a22e6933ad5c0e76ab4161d87db
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-26 08:06:52 -0400

    build: install and config jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  preset: 'ts-jest',
+  testMatch: ['**/*.spec.ts'],
+};
